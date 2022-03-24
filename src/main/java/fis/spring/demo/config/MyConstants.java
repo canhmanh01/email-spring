7344ffdd9941dca497d3a9cbfecc9b178733b213
canhmanh01 <86338206+canhmanh01@users.noreply.github.com>
2022-03-24 22:39:00 +0700

    load

---
+++ src/main/java/fis/spring/demo/config/MyConstants.java
@@ -0,0 +1,14 @@
+package fis.spring.demo.config;
+
+public class MyConstants {
+
+    // Replace with your email here:  
+    public static final String MY_EMAIL = "dangvancanh19999@gmail.com";
+
+    // Replace password!!
+    public static final String MY_PASSWORD = ".....";
+
+    // And receiver!
+    public static final String FRIEND_EMAIL = "dangvancanh19999@gmail.com";
+
+}
---
+++ src/test/java/fis/spring/demo/EmailDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package fis.spring.demo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmailDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
